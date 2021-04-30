d4a716b64249aeafbb962c7eda0c4319cc054528
Computer Vision&Machine Learning Lab <73872706+cvmllab@users.noreply.github.com>
2021-04-30 10:35:04 +0900

    Add files via upload

---
+++ train.m
@@ -0,0 +1,53 @@
+clear
+clc
+%% set gpu
+delete(gcp('nocreate'));
+numberOfWorkers = 2;
+parpool(numberOfWorkers);
+
+%% load data
+image = imageDatastore('./data',...
+                      'IncludeSubfolders',true,...
+                      'LabelSource','foldernames',...
+                      'FileExtensions',{'.jpg'});
+rng(0);
+image = shuffle(image);
+
+% Divide the data into training and validation data sets. Use 70% of the images for training and 30% for validation. splitEachLabel splits the images datastore into two new datastores.
+[trainingImages,validationImages] = splitEachLabel(image, 0.7);
+numTrainImages = numel(trainingImages.Labels);
+
+%% image resize (Resizing image 224 224)
+trainingImages.ReadFcn = @customReadDatastoreImage;
+validationImages.ReadFcn = @customReadDatastoreImage;
+
+%% load network (untrained network)
+load('./network/VGG16_seg_ver2_fcn_2weight_trainbefor.mat');
+lgraph = lgraph_1; 
+
+%% train
+miniBatchSize = 10;
+numIterationsPerEpoch = floor(numTrainImages/miniBatchSize);
+options = trainingOptions('sgdm',...
+    'MiniBatchSize',miniBatchSize,...
+    'MaxEpochs', 30,...
+    'Verbose',true,...
+    'InitialLearnRate',1e-4,...
+    'Plots','training-progress',...
+    'ValidationData',validationImages,...
+    'ExecutionEnvironment','parallel',....
+    'ValidationFrequency',numIterationsPerEpoch);
+
+netTransfer = trainNetwork(trainingImages, lgraph, options);
+%% save network
+save('./network/result_VGG16_seg_ver2_fcn_2weight_trainbefor.mat', 'netTransfer')
+
+%%
+function data = customReadDatastoreImage(filename)
+% code from default function: 
+onState = warning('off', 'backtrace'); 
+c = onCleanup(@() warning(onState)); 
+data = imread(filename); % added lines: 
+data = data(:,:,min(1:3, end)); 
+data = imresize(data,[224 224], 'method', 'bilinear');
+end
